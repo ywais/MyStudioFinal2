2861e554acf41e2bbdef13615de4d0b64f65dfb7
ywais <67857593+ywais@users.noreply.github.com>
2021-06-03 08:34:10 +0300

    update photos links

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,7 +2,7 @@ function Home() {
   return (
     <div className='homePage'>
       <div className='homeStrip'>
-        <img className='homeImage' src='https://instagram.fiev22-1.fna.fbcdn.net/v/t51.2885-15/e35/19424892_1833204223664103_111733909212889088_n.jpg?tp=1&_nc_ht=instagram.fiev22-1.fna.fbcdn.net&_nc_cat=103&_nc_ohc=VWBbPQhiT_QAX_W1j-j&edm=AP_V10EAAAAA&ccb=7-4&oh=27fe7c41b23ac8b9e4479b0f906eb117&oe=6092B2BA&_nc_sid=4f375e' alt='kintos' />
+        <img className='homeImage' src='https://cdn3.dumpor.com/view?q=%3D%3DwMwYDZzgTPkl2cfNmbfZSQ3kTOFJEM20TZvZiYhJGZlZTM5kjYmVzMlNzN2czM5YmYkN2YihTYjdjMw0DavZCNtcTPiN2YmEUQBFkQVpmdCJUQB1TbkVmJxoXQwATOYFUVPJFeod2V4EnYf1zYo92Xj52XmMDMx0DdhN2Xj52XmQXZu5ibkNmYm5SYuZmLx0iNsVGam5SbhJ3ZhR3culWP0h2Xj52XmETPwR3PnBnau42X4gDM5gDOyEjM5ATOzMzNxETMfNDMxQjN2MjMyQDMyMzM4EzXykDO0IDN5EzL1MTZvUTMtUDO4IjLxUDdvY3L0Vmbu4GZjJmZuEmbm5SMtYDblhmZu0WYydWY0Nnbp9yL6MHc0RHa' alt='kintos' />
         <img className='homeImage homeTorn' id='homeTornFirst' src='torn_bottom.png' alt='torn bottom' />
         <div className='homeStripTextContainer'>
           <h2 className='homeStripText'>תיפוף גוף, תיפוף על פחים,</h2>
@@ -11,7 +11,7 @@ function Home() {
           <h1 className='homeStripText'>אך הכי חשוב ביחד !!!</h1>
         </div>
         <img className='homeImage homeTorn' id='homeTornSecond' src='torn_top.png' alt='torn top' />
-        <img className='homeImage' src='https://instagram.flwo4-2.fna.fbcdn.net/v/t51.2885-15/e35/37576552_1122354481237959_2675074449933336576_n.jpg?tp=1&_nc_ht=instagram.flwo4-2.fna.fbcdn.net&_nc_cat=103&_nc_ohc=JPs_kU30FZwAX83srh0&edm=AP_V10EAAAAA&ccb=7-4&oh=5df9cc3b4362861b6609c44f49e52f58&oe=609537A3&_nc_sid=4f375e' alt='baruch' />
+        <img className='homeImage' src='https://cdn3.dumpor.com/view?q=%3DMDM2Q2M40DZpN3Xj52XmMjN5gjRCBjN9U2bmUWZyIzN1cTZyUmMiZWYjlTZihTYyQzM2QTNiZWNkFWM9g2bmQTL30jYjNmJBFUQBJUVqZnQCFUQ90GZlZCWLh3RshDWBdnZqljRjRUZO5UT9MGav91Yu9lJzATM9QXYj91Yu9lJ0Vmbu4GZjJmZuEmbm5SMtYDblhmZu0WYydWY0Nnbp1Ddo91Yu9lJx0Dc09zZwpmLu9lN3UjNzMzMzkTO0QDN3ATN3YjMflTN5czMyEDO0QTNzIjMxEzXyUTN2cTN3MzL1MTZvUTMtUDO4IjLxUDdvY3L0Vmbu4GZjJmZuEmbm5SMtYDblhmZu0WYydWY0Nnbp9yL6MHc0RHa' alt='baruch' />
         <img className='homeImage homeTorn' id='homeTornThird' src='torn_bottom.png' alt='torn bottom' />
         <div className='homeStripTextContainer'>
           <h2 className='homeStripText'>להקות "דרך הקצב"</h2>
@@ -20,7 +20,7 @@ function Home() {
           <h3 className='homeStripText'>הלהקות מופיעות בהפקות מרכזיות בארץ ובחו"ל</h3>
         </div>
         <img className='homeImage homeTorn' id='homeTornFourth' src='torn_top.png' alt='torn top' />
-        <img className='homeImage' src='https://instagram.flwo4-2.fna.fbcdn.net/v/t51.2885-15/e35/36963204_215272745993956_51307559058931712_n.jpg?tp=1&_nc_ht=instagram.flwo4-2.fna.fbcdn.net&_nc_cat=104&_nc_ohc=ewpdy-LERJgAX-yuNTQ&edm=AP_V10EAAAAA&ccb=7-4&oh=a7002a642cfee8fe344943c76ad07e74&oe=6095E1B3&_nc_sid=4f375e' alt='emet' />
+        <img className='homeImage' src='https://cdn3.dumpor.com/view?q=%3DMDM2Q2M40DZpN3Xj52XmMzNzMDMDBjN9U2bmgTZxETY0IjM4YGO1UmZyUTN2IzM2IjNldTZlFjZ1E2Y9g2bmQTL30jYjNmJBFUQBJUVqZnQCFUQ90GZlZiYVNja40CWBdmdRRXWK9lehNGW9MGav91Yu9lJ0ATM9QXYj91Yu9lJ0Vmbu4GZjJmZuEmbm5SMtMDblhmZu0WYydWY0Nnbp1Ddo91Yu9lJx0Dc09zZwpmLu9lMxcTMzkDO1ATO1UzNwMTM18lN1kzM5kTN0cjM3ITNxIzX0AjMzYTO2MzL1MTZvUTMtUDO4IjLxUDdvY3L0Vmbu4GZjJmZuEmbm5SMtMDblhmZu0WYydWY0Nnbp9yL6MHc0RHa' alt='emet' />
       </div>
     </div>
   );
